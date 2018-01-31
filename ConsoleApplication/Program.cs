7b577686bd73234d0f89a7442ac76877a9efeb46
mkonopkait <mkonopka.it@gmail.com>
2018-01-31 17:39:13 +0100

    pierwszy

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+            Console.WriteLine("Witaj");
         }
     }
 }
